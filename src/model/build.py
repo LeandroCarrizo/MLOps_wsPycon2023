527c75e21b4fff63c5e8bb41080d39f37f229a02
Leandro Carrizo <101889418+LeandroCarrizo@users.noreply.github.com>
2023-06-13 19:00:22 -0300

    Update build.py

--- src/model/build.py
+++ src/model/build.py
@@ -7,6 +7,7 @@ import os
 import argparse
 import wandb
 
+#test
 parser = argparse.ArgumentParser()
 parser.add_argument('--IdExecution', type=str, help='ID of the execution')
 args = parser.parse_args()
